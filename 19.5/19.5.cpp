d6f1edaf3f8b43ad93e5b241a3237af28b0b0fc4
alex <a>
2024-04-13 12:07:27 +0400

    Добавьте файлы проекта.

---
+++ 19.5/19.5.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+    virtual void Voice()
+    {
+        std::cout << "Why???";
+    }
+};
+
+class Cat : public Animal
+{
+    void Voice() override
+    {
+        std::cout << "Cat: Meau :3";
+    }
+};
+
+class Dog : public Animal
+{
+    void Voice() override
+    {
+        std::cout << "Dog: Wooof !!!";
+    }
+};
+
+class Owl : public Animal
+{
+    void Voice() override
+    {
+        std::cout << "Owl: Uuuu~Uuuu...";
+    }
+};
+
+int main() 
+{
+    Animal** p = new Animal*[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (i == 0)
+        {
+            p[i] = new Cat;
+        }
+        else if (i == 1)
+        {
+            p[i] = new Dog;
+        }
+        else
+        {
+            p[i] = new Owl;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        p[i]->Voice();
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        delete p[i];
+    }
+
+    delete[] p;
+
+}
